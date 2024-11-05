7b05ccec58e3be833a37cb99d97be004defa91a2
MiaGMB <mre42348@stud.hs-furtwangen.de>
2024-11-05 15:44:20 +0100

    added first fudge

--- FirstFudge/FirstFudge.js
+++ FirstFudge/FirstFudge.js
@@ -1,10 +1,28 @@
 "use strict";
 var FirstFudge;
 (function (FirstFudge) {
+    var F = FudgeCore;
     window.addEventListener("load", start);
+    function start(_event) {
+        const canvas = document.querySelector("canvas");
+        const mesh = new F.MeshCube("cube");
+        const camera = new F.ComponentCamera();
+        const node = new F.Node("Node");
+        const cmpMesh = new F.ComponentMesh(mesh);
+        node.addComponent(cmpMesh);
+        const material = new F.Material("Material", F.ShaderLit);
+        const cmpMaterial = new F.ComponentMaterial(material);
+        cmpMaterial.clrPrimary.set(1, 0, 0, 5);
+        node.addComponent(cmpMaterial);
+        camera.mtxPivot.translateZ(5);
+        camera.mtxPivot.rotateY(180);
+        const cmpTransform = new F.ComponentTransform();
+        node.addComponent(cmpTransform);
+        //node.getComponent(F.ComponentTransform).mtxLocal.translateX(2);
+        node.mtxLocal.translateX(2);
+        console.log(camera);
+        const viewport = new F.Viewport();
+        viewport.initialize("Viewport", node, camera, canvas);
+        viewport.draw();
+    }
 })(FirstFudge || (FirstFudge = {}));
-;
-function start(_event) {
-    const canvas = document.querySelector("canvas");
-    console.log(canvas);
-}
--- FirstFudge/FirstFudge.ts
+++ FirstFudge/FirstFudge.ts
@@ -1,9 +1,37 @@
 namespace FirstFudge {
     import F = FudgeCore;
-    
-    window.addEventListener("load", start)};
+
+    window.addEventListener("load", start);
 
     function start(_event: Event): void {
         const canvas: HTMLCanvasElement = document.querySelector("canvas")!;
-        console.log(canvas);
+
+        const mesh: F.MeshCube = new F.MeshCube("cube");
+
+        const camera: F.ComponentCamera = new F.ComponentCamera();
+
+        const node: F.Node = new F.Node("Node");
+
+        const cmpMesh: F.ComponentMesh = new F.ComponentMesh(mesh);
+        node.addComponent(cmpMesh);
+
+        const material: F.Material = new F.Material("Material", F.ShaderLit);
+        const cmpMaterial: F.ComponentMaterial = new F.ComponentMaterial(material);
+        cmpMaterial.clrPrimary.set(1, 0, 0, 5);
+        node.addComponent(cmpMaterial);
+
+        camera.mtxPivot.translateZ(5);
+        camera.mtxPivot.rotateY(180)
+
+        const cmpTransform: F.ComponentTransform = new F.ComponentTransform();
+        node.addComponent(cmpTransform);
+        //node.getComponent(F.ComponentTransform).mtxLocal.translateX(2);
+        node.mtxLocal.translateX(2);
+
+        console.log(camera);
+
+        const viewport: F.Viewport = new F.Viewport();
+        viewport.initialize("Viewport", node, camera, canvas);
+        viewport.draw();
     }
+}
